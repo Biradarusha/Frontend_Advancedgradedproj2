c07b382071f7b7a4c23f7e7d3cb83a98c1e55fce
Biradarusha <101283209+Biradarusha@users.noreply.github.com>
2022-10-30 11:46:41 +0530

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -2,10 +2,10 @@ function login() {
     let username = document.getElementById('user-input').value;
     let password = document.getElementById('password-input').value;
      // Storing username & password in localStorage, overriding them if they exist
-    window.localStorage.setItem('username', 'username');
-    window.localStorage.setItem('password', 'password');
+    window.localStorage.setItem('username', 'usha');
+    window.localStorage.setItem('password', 'usha123');
     // Will be an API call in production
-    if(username === 'username' && password === 'password') {
+    if(username === 'usha' && password === 'usha123') {
         window.location = 'resume.html';
     } else {
         document.getElementById('invalid-login').style.display = 'block';
